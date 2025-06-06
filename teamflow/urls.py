7d6869d8315a6735c6fef55c4b9e44ab72ce4aa7
boiyelove <daahrmmieboiye@gmail.com>
2025-06-06 16:23:18 +0100

    Update URL patterns to use path() instead of url()

--- teamflow/urls.py
+++ teamflow/urls.py
@@ -1,20 +1,20 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = "teamflow"
 urlpatterns = [
-	url(r'^create/$', views.NewUnconfirmedUser.as_view(), name='get-invited'),
-	url(r'^create/team/$', views.CreateTeam.as_view(), name='create-team'),
-	url(r'^create/(?P<slug>[\w]+)/$', views.CreateAccount.as_view(), name='create-account'),
-	url(r'^teams/$', views.TeamList.as_view(), name='team-profile'),	
-	url(r'^teams/(?P<team_slug>[\w]+)/$', views.TeamDetail.as_view(), name='team-single'),	
-	url(r'^teams/(?P<team_slug>[\w]+)/members/$', views.TeamMemberList.as_view(), name='team-members'),
-	url(r'^teams/(?P<team_slug>[\w]+)/members/(?P<member_slug>[\w]+)/$', views.TeamMemberDetail.as_view(), name='team-member'),
+	path('create/', views.NewUnconfirmedUser.as_view(), name='get-invited'),
+	path('create/team/', views.CreateTeam.as_view(), name='create-team'),
+	path('create/<slug:slug>/', views.CreateAccount.as_view(), name='create-account'),
+	path('teams/', views.TeamList.as_view(), name='team-profile'),	
+	path('teams/<slug:team_slug>/', views.TeamDetail.as_view(), name='team-single'),	
+	path('teams/<slug:team_slug>/members/', views.TeamMemberList.as_view(), name='team-members'),
+	path('teams/<slug:team_slug>/members/<slug:member_slug>/', views.TeamMemberDetail.as_view(), name='team-member'),
 	
-	url(r'^teams/(?P<team_slug>[\w]+)/flows/$', views.TeamMemberDetail.as_view(), name='team-flows'),
-	url(r'^teams/(?P<team_slug>[\w]+)/flows/(?P<slug>[\w]+)/$', views.TeamMemberDetail.as_view(), name='team-flow'),
+	path('teams/<slug:team_slug>/flows/', views.TeamMemberDetail.as_view(), name='team-flows'),
+	path('teams/<slug:team_slug>/flows/<slug:slug>/', views.TeamMemberDetail.as_view(), name='team-flow'),
 	
-	url(r'^invited/(?P<slug>[\w]+)/$', views.JoinTeam.as_view),
-	url(r'^login/$', views.LoginView.as_view(), name="login"),
-	url(r'^logout/$', views.LogoutView.as_view(), name="logout"),
+	path('invited/<slug:slug>/', views.JoinTeam.as_view, name='join-team'),
+	path('login/', views.LoginView.as_view(), name="login"),
+	path('logout/', views.LogoutView.as_view(), name="logout"),
 	]
--- webcore/urls.py
+++ webcore/urls.py
@@ -1,43 +1,43 @@
-from django.conf.urls import url, handler400, handler403, handler404, handler500
+from django.urls import path
 from . import views
 
 #Custom Error Pages Routes
-handler400 = views.handler400
-handler403 = views.handler403
-handler404 = views.handler404
-handler500 = views.handler500
+handler400 = 'webcore.views.handler400'
+handler403 = 'webcore.views.handler403'
+handler404 = 'webcore.views.handler404'
+handler500 = 'webcore.views.handler500'
 
 app_name = 'webcore'
 urlpatterns = [
 
 	#Default Pages Routes
-	url(r'^$',  views.HomePageView.as_view(), name='home-page'),
-	url(r'^aboutus/$', views.AboutUsPageView.as_view(), name = 'about-us'),
-	url(r'^contactus/$', views.ContactUsPageView.as_view(), name = 'contact-us'),
+	path('',  views.HomePageView.as_view(), name='home-page'),
+	path('aboutus/', views.AboutUsPageView.as_view(), name = 'about-us'),
+	path('contactus/', views.ContactUsPageView.as_view(), name = 'contact-us'),
 	
 	
 	# #Email Campaign
-	# url(r'^subscribe/$', views.AddEmailVerification.as_view(), name = 'verify-email-form'),
-	# url(r'^subscribe/verify/(?P<verification_key>[\w-]+)/$', views.CheckEmailVerification.as_view(), name = 'verify-email-status'),
-	# url(r'^subscribe/(?P<subscriber_id>\d+)/add/(?P<campaign_list>\d+)/$', views.UpdateNewsletterSubscription.as_view(), name = 'add-to-campaign'),
-	# url(r'^unsubscribe/(?P<subscriber_id>\d+)/add/(?P<campaign_list>\d+)/$', views.UpdateNewsletterSubscription.as_view(), name = 'remove-from-campaign'),	#Banner
+	# path('subscribe/', views.AddEmailVerification.as_view(), name = 'verify-email-form'),
+	# path('subscribe/verify/<str:verification_key>/', views.CheckEmailVerification.as_view(), name = 'verify-email-status'),
+	# path('subscribe/<int:subscriber_id>/add/<int:campaign_list>/', views.UpdateNewsletterSubscription.as_view(), name = 'add-to-campaign'),
+	# path('unsubscribe/<int:subscriber_id>/add/<int:campaign_list>/', views.UpdateNewsletterSubscription.as_view(), name = 'remove-from-campaign'),	#Banner
 
 	
 	#Category
-	url(r'^category/$', views.CategoryListView.as_view(), name="webcore-category-List"),
-	url(r'^category/add/$', views.CategoryCreateView.as_view(), name="webcore-category-create"),
-	url(r'^category/(?P<slug>[\w-]+)/$', views.CategoryDetailView.as_view(), name="webcore-category-detail"),
-	url(r'^category/(?P<slug>[\w-]+)/edit/$', views.CategoryUpdateView.as_view(), name="webcore-category-edit"),
-	url(r'^category/(?P<slug>[\w-]+)/delete/$', views.CategoryDeleteView.as_view(), name="webcore-category-delete"),
-	url(r'^categpry/(?P<pk>[\d]+)/$', views.CategoryDetailView.as_view(), name ="webcore-category-pk"),	
-	url(r'^check_user_exists/$', views.UserCheckView.as_view(), name ="webcore-user-check"),
+	path('category/', views.CategoryListView.as_view(), name="webcore-category-List"),
+	path('category/add/', views.CategoryCreateView.as_view(), name="webcore-category-create"),
+	path('category/<slug:slug>/', views.CategoryDetailView.as_view(), name="webcore-category-detail"),
+	path('category/<slug:slug>/edit/', views.CategoryUpdateView.as_view(), name="webcore-category-edit"),
+	path('category/<slug:slug>/delete/', views.CategoryDeleteView.as_view(), name="webcore-category-delete"),
+	path('categpry/<int:pk>/', views.CategoryDetailView.as_view(), name ="webcore-category-pk"),	
+	path('check_user_exists/', views.UserCheckView.as_view(), name ="webcore-user-check"),
 
 	#Pages
-	# url(r'^create_page/$', views.PageCreateView.as_view(), name="webcore-page-create"),
-	# url(r'^(?P<slug>[\w-]+)/$', views.PageDetailView.as_view(), name="webcore-page-detail"),
-	# url(r'^(?P<slug>[\w-]+)/edit/$', views.PageUpdateView.as_view(), name="webcore-page-edit"),
-	# url(r'^(?P<slug>[\w-]+)/delete/$', views.PageDeleteView.as_view(), name="webcore-page-delete"),
-	# url(r'^(?P<pk>[\d]+)/$', views.PageDetailView.as_view(), name ="webcore-page-pk"),
+	# path('create_page/', views.PageCreateView.as_view(), name="webcore-page-create"),
+	# path('<slug:slug>/', views.PageDetailView.as_view(), name="webcore-page-detail"),
+	# path('<slug:slug>/edit/', views.PageUpdateView.as_view(), name="webcore-page-edit"),
+	# path('<slug:slug>/delete/', views.PageDeleteView.as_view(), name="webcore-page-delete"),
+	# path('<int:pk>/', views.PageDetailView.as_view(), name ="webcore-page-pk"),
 
 
 
